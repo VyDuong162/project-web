166da87ec1a222af76b80c797245a5855e51612d
VyDuong162 <69305591+VyDuong162@users.noreply.github.com>
2020-08-27 13:30:45 +0700

    formtk

--- form-timkiem.php
+++ form-timkiem.php
@@ -8,20 +8,20 @@
 </head>
 <body>
   <h1>Form Tìm Kiếm</h1>
-  <form name="frm-timkiem" id="frm-timkiem" action="xuly-timkiem.php" method="get">
+  <form name="frm-timkiem" id="frm-timkiem" action="xuly-timkiem.php" method="GET">
   <h2><b>Tên sản phẩm</b> </h2>
   <input type="text" name="txttimkiem" id="txttimkiem"><br>
-  <h2>Loại sản phẩm</h2><br>
-  <input type="checkbox" name="chkbloaisanpham[]" id="chkbloaisanpham-0" value="0">Máy Tính bảng <br>
-  <input type="checkbox" name="chkbloaisanpham[]" id="chkbloaisanpham-1" value="1">Máy tính xách tay <br>
-  <input type="checkbox" name="chkbloaisanpham[]" id="chkbloaisanpham-2" value="2">Điên Thoại <br>
-  <input type="checkbox" name="chkbloaisanpham[]" id="chkbloaisanpham-3" value="3">Phụ kiện <br>
-  <h2>Nhà sản xuất</h2><br>
-  <input type="checkbox" name="chkbnhasanxuat[]" id="chkbnhasanxuat-0" value="0">Apple <br>
-  <input type="checkbox" name="chkbnhasanxuat[]" id="chkbnhasanxuat-1" value="1">Samsung  <br>
-  <input type="checkbox" name="chkbnhasanxuat[]" id="chkbnhasanxuat-2" value="2">HTC<br>
-  <input type="checkbox" name="chkbnhasanxuat[]" id="chkbnhasanxuat-3" value="3">Nokia<br>
-  <h2>Khuyến mãi</h2><br>
+  <h3>Loại sản phẩm</h3><br>
+  <input type="checkbox" name="chkbloaisanpham[]" id="chkbloaisanpham-1" value="1">Máy Tính bảng <br>
+  <input type="checkbox" name="chkbloaisanpham[]" id="chkbloaisanpham-2" value="2">Máy tính xách tay <br>
+  <input type="checkbox" name="chkbloaisanpham[]" id="chkbloaisanpham-3" value="3">Điên Thoại <br>
+  <input type="checkbox" name="chkbloaisanpham[]" id="chkbloaisanpham-4" value="4">Phụ kiện <br>
+  <h3>Nhà sản xuất</h3><br>
+  <input type="checkbox" name="chkbnhasanxuat[]" id="chkbnhasanxuat-1" value="1">Apple <br>
+  <input type="checkbox" name="chkbnhasanxuat[]" id="chkbnhasanxuat-2" value="2">Samsung  <br>
+  <input type="checkbox" name="chkbnhasanxuat[]" id="chkbnhasanxuat-3" value="3">HTC<br>
+  <input type="checkbox" name="chkbnhasanxuat[]" id="chkbnhasanxuat-4" value="4">Nokia<br>
+  <h3>Khuyến mãi</h3><br>
   <input type="radio" name="rdkhuyenmai" id="rdkhuyenmai" value="0">20% <br>
   <input type="radio" name="rdkhuyenmai" id="rdkhuyenmai" value="1">50% <br>
   <br>
--- xuly-timkiem.php
+++ xuly-timkiem.php
@@ -1,7 +1,32 @@
 <?php
- 
+ if(isset($_GET['subgui'])){
  if(isset($_GET['txttimkiem'])){
       $ten_sanpham=$_GET['txttimkiem'];
  }
-   
+$loaisanpham=[];
+ if(isset($_GET['chkbloaisanpham'])){
+      $loaisanpham=$_GET['chkbloaisanpham'];
+ }
+ $nhasanxuat=[];
+ if(isset($_GET['chkbnhasanxuat'])){
+      $nhasanxuat=$_GET['chkbnhasanxuat'];
+ }
+ 
+ $khuyenmai=null;
+ if(isset($_GET['rdkhuyenmai'])){
+      $khuyenmai=$_GET['rdkhuyenmai'];
+ }
+ echo "<ul>";
+ echo "<li>Từ khóa tìm kiếm: <b>{$ten_sanpham}</b></li>";
+ if(!empty($loaisanpham)){
+      $loaisanpham_string=implode(',',$loaisanpham);
+      echo "<li>Các loại sản phẩm tìm kiếm: <b>{$loaisanpham_string}</b></li>";
+ }
+ if(!empty($nhasanxuat)){
+      $nhasanxuat_string=implode(',',$nhasanxuat);
+      echo "<li>Các Nhà sản xuất tìm kiếm: <b>{$nhasanxuat_string}</b></li>";
+ }
+      echo "<li>Khuyến mãi:<b>{$khuyenmai}</b></li>";
+ echo "</ul>";
+}
 ?>
