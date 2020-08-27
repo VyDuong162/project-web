df5abee5b7bc689ca7d46d52cbc41788b7f13e02
VyDuong162 <69305591+VyDuong162@users.noreply.github.com>
2020-08-27 14:02:47 +0700

    up buoi 7

---
+++ backend/dbvonglap.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>While PHP</h1>
+    <?php
+        include_once(__DIR__.'/../dbconnect.php');
+      // 2. Query
+      //here doc
+        $sql=<<<EOT
+        SELECT httt_ma , httt_ten FROM `hinhthucthanhtoan`;
+
+EOT;
+    //3. Yêu cầu PHP thực thi query 
+    $result = mysqli_query($conn,$sql);
+    //4. tạo mảng chứa dữ liệu
+    $data =[];
+    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+        $data[] = array(
+            'ma'=> $row['httt_ma'],
+            'ten'=> $row['httt_ten'],
+        );
+    }
+    ?>
+    <table border="1">
+        <tr>
+            <th>Mã Thanh Toán</th>
+            <th>Tên Thanh Toán</th>
+        </tr>
+        <tr>
+            <?php foreach($data as $bien ) :?>
+                <td><?= $httt['ma']; ?></td>
+                <td><?= $httt['ten']; ?></td>
+            <?php endforeach; ?>
+        </tr>
+    </table>
+</body>
+</html>
