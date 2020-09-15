8cbda9ad653c881b9445216584e89018da0eec19
STUDENT <vyduong162@gmail.com>
2020-09-15 19:23:12 +0700

    up api + charjs

---
+++ backend/pages/dashboard1.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LAYOUT-bạckend</title>
+    <?php include_once(__DIR__.'/../layouts/style.php');?>
+</head>
+<body>
+    <?php include_once(__DIR__.'/../layouts/partials/header.php');?>
+    <div class="container-fluid">
+        <div class="row">
+    <?php include_once(__DIR__.'/../layouts/partials/sildebar.php');?>
+        <div class="col-md-8">
+        <H1>DASHBOARD</H1>
+             <!-- Block content -->
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-6 col-lg-3">
+              <div class="card text-white bg-primary mb-2">
+                <div class="card-body pb-0">
+                  <div class="text-value" id="baocaoSanPham_SoLuong">
+                    <h1>0</h1>
+                  </div>
+                  <div>Tổng số mặt hàng</div>
+                </div>
+              </div>
+              <button class="btn btn-primary btn-sm form-control" id="refreshBaoCaoSanPham">Refresh dữ liệu</button>
+            </div> <!-- Tổng số mặt hàng -->
+            <div class="col-sm-6 col-lg-3">
+              <div class="card text-white bg-success mb-2">
+                <div class="card-body pb-0">
+                  <div class="text-value" id="baocaoKhachHang_SoLuong">
+                    <h1>0</h1>
+                  </div>
+                  <div>Tổng số khách hàng</div>
+                </div>
+              </div>
+              <button class="btn btn-success btn-sm form-control" id="refreshBaoCaoKhachHang">Refresh dữ liệu</button>
+            </div> <!-- Tổng số khách hàng -->
+            <div class="col-sm-6 col-lg-3">
+              <div class="card text-white bg-warning mb-2">
+                <div class="card-body pb-0">
+                  <div class="text-value" id="baocaoDonHang_SoLuong">
+                    <h1>0</h1>
+                  </div>
+                  <div>Tổng số đơn hàng</div>
+                </div>
+              </div>
+              <button class="btn btn-warning btn-sm form-control" id="refreshBaoCaoDonHang">Refresh dữ liệu</button>
+            </div> <!-- Tổng số đơn hàng -->
+            <div class="col-sm-6 col-lg-3">
+              <div class="card text-white bg-danger mb-2">
+                <div class="card-body pb-0">
+                  <div class="text-value" id="baocaoGopY_SoLuong">
+                    <h1>0</h1>
+                  </div>
+                  <div>Tổng số góp ý</div>
+                </div>
+              </div>
+              <button class="btn btn-danger btn-sm form-control" id="refreshBaoCaoGopY">Refresh dữ liệu</button>
+            </div> <!-- Tổng số góp ý -->
+            <div id="ketqua"></div>
+          </div><!-- row -->
+          <div class="row">
+            <!-- Biểu đồ thống kê loại sản phẩm -->
+            <div class="col-sm-6 col-lg-6">
+              <canvas id="chartOfobjChartThongKeLoaiSanPham"></canvas>
+              <button class="btn btn-outline-primary btn-sm form-control" id="refreshThongKeLoaiSanPham">Refresh dữ liệu</button>
+            </div><!-- col -->
+          </div><!-- row -->
+        </div>
+        <!-- End block content -->
+        </div>
+    </div>
+
+
+    </div>
+    <?php include_once(__DIR__.'/../layouts/partials/footer.php');?>
+    <?php include_once(__DIR__.'/../layouts/scripts.php');?>
+    <!-- Liên kết thư viện ChartJS -->
+    <script src="/project-web/assets/vendor/Chart.js/Chart.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            function getDuLieuTongSoMatHang(){
+                $.ajax('/project-web/backend/api/baocao-tongsomathang.php',{
+                success: function(data){
+                    var dataobj = JSON.parse(data);
+                    var htmlstring =  `<h1>${dataobj.SoLuong}</h1>`;
+                    $('#baocaoSanPham_SoLuong').html(htmlstring);
+                    },
+                error: function() {
+                    var htmlString = `<h1>Không thể xử lý</h1>`;
+                    $('#baocaoSanPham_SoLuong').html(htmlString);
+                    }
+                });
+            }
+            $('#refreshBaoCaoSanPham').click(function(){
+                getDuLieuTongSoMatHang();
+            });
+       
+            // ----------------- Tổng số khách hàng --------------------------
+            function getDuLieuBaoCaoTongSoKhachHang() {
+                $.ajax('/project-web/backend/api/baocao-tongsokhachhang.php', {
+                success: function(data) {
+                    var dataObj = JSON.parse(data);
+                    var htmlString = `<h1>${dataObj.SoLuong}</h1>`;
+                    $('#baocaoKhachHang_SoLuong').html(htmlString);
+                },
+                error: function() {
+                    var htmlString = `<h1>Không thể xử lý</h1>`;
+                    $('#baocaoKhachHang_SoLuong').html(htmlString);
+                }
+                });
+            }
+            $('#refreshBaoCaoKhachHang').click(function(event) {
+                event.preventDefault();
+                getDuLieuBaoCaoTongSoKhachHang();
+            });
+            // ----------------- Tổng số đơn hàng --------------------------
+            function getDuLieuBaoCaoTongSoDonHang() {
+                $.ajax('/project-web/backend/api/baocao-tongsoluongdonhang.php', {
+                success: function(data) {
+                    var dataObj = JSON.parse(data);
+                    var htmlString = `<h1>${dataObj.SoLuong}</h1>`;
+                    $('#baocaoDonHang_SoLuong').html(htmlString);
+                },
+                error: function() {
+                    var htmlString = `<h1>Không thể xử lý</h1>`;
+                    $('#baocaoDonHang_SoLuong').html(htmlString);
+                }
+                });
+            }
+            $('#refreshBaoCaoDonHang').click(function(event) {
+                event.preventDefault();
+                getDuLieuBaoCaoTongSoDonHang();
+            });
+            // ----------------- Tổng số Góp ý --------------------------
+            function getDuLieuBaoCaoTongSoGopY() {
+                $.ajax('/project-web/backend/api/baocao-tongsogopy.php', {
+                success: function(data) {
+                    var dataObj = JSON.parse(data);
+                    var htmlString = `<h1>${dataObj.SoLuong}</h1>`;
+                    $('#baocaoGopY_SoLuong').html(htmlString);
+                },
+                error: function() {
+                    var htmlString = `<h1>Không thể xử lý</h1>`;
+                    $('#baocaoGopY_SoLuong').html(htmlString);
+                }
+                });
+            }
+            $('#refreshBaoCaoGopY').click(function(event) {
+                event.preventDefault();
+                getDuLieuBaoCaoTongSoGopY();
+            });
+            // ------------------ Vẽ biểu đồ thống kê Loại sản phẩm -----------------
+            // Vẽ biểu đổ Thống kê Loại sản phẩm sử dụng ChartJS
+            var $objChartThongKeLoaiSanPham;
+            var $chartOfobjChartThongKeLoaiSanPham = document.getElementById("chartOfobjChartThongKeLoaiSanPham").getContext(
+                "2d");
+            function renderChartThongKeLoaiSanPham() {
+                $.ajax({
+                url: '/project-web/backend/api/baocao-thongkeloaisanpham.php',
+                type: "GET",
+                success: function(response) {
+                    var data = JSON.parse(response);
+                    var myLabels = [];
+                    var myData = [];
+                    $(data).each(function() {
+                    myLabels.push((this.TenLoaiSanPham));
+                    myData.push(this.SoLuong);
+                    });
+                    myData.push(0); // tạo dòng số liệu 0
+                    if (typeof $objChartThongKeLoaiSanPham !== "undefined") {
+                    $objChartThongKeLoaiSanPham.destroy();
+                    }
+                    $objChartThongKeLoaiSanPham = new Chart($chartOfobjChartThongKeLoaiSanPham, {
+                    // Kiểu biểu đồ muốn vẽ. Các bạn xem thêm trên trang ChartJS
+                    type: "bar",
+                    data: {
+                        labels: myLabels,
+                        datasets: [{
+                        data: myData,
+                        borderColor: "#9ad0f5",
+                        backgroundColor: "#9ad0f5",
+                        borderWidth: 1
+                        }]
+                    },
+                    // Cấu hình dành cho biểu đồ của ChartJS
+                    options: {
+                        legend: {
+                        display: false
+                        },
+                        title: {
+                        display: true,
+                        text: "Thống kê Loại sản phẩm"
+                        },
+                        responsive: true
+                    }
+                    });
+                }
+                });
+            };
+            $('#refreshThongKeLoaiSanPham').click(function(event) {
+                event.preventDefault();
+                renderChartThongKeLoaiSanPham();
+            });
+            // Mới mở web (khi trang web load xong)
+            // thì sẽ gọi lập tức một số hàm AJAX gọi API lấy dữ liệu
+            getDuLieuTongSoMatHang();
+            getDuLieuBaoCaoTongSoKhachHang();
+            getDuLieuBaoCaoTongSoDonHang();
+            getDuLieuBaoCaoTongSoGopY();
+            renderChartThongKeLoaiSanPham();
+        });  
+    </script>
+</body>
+</html>
