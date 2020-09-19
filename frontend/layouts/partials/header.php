1f35b172ffaf712ad96f79551e33cda4876941b7
STUDENT <vyduong162@gmail.com>
2020-09-19 19:07:10 +0700

    mail

---
+++ frontend/layouts/partials/header.php
@@ -0,0 +1,30 @@
+<?php
+  if (session_id() === '') {
+    // Yêu cầu Web Server tạo file Session để lưu trữ giá trị tương ứng với CLIENT (Web Browser đang gởi Request)
+    session_start();
+}
+?>
+<nav class="navbar navbar-dark bg-dark shadow">
+  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/">Nền tảng</a>
+  <ul class="navbar-nav px-3 mr-auto">
+    <li class="nav-item text-nowrap">
+      <a class="nav-link" href="/backend/pages/dashboard.php">Bảng tin</a>
+    </li>
+  </ul>
+      <?php
+      // Đã đăng nhập rồi -> hiển thị tên Người dùng và menu Đăng xuất
+      if (isset($_SESSION['kh_tendangnhap_logged']) && !empty($_SESSION['kh_tendangnhap_logged'])) :
+      ?>
+        <li class="nav-item text-nowrap">
+          <a class="nav-link">Chào <?= $_SESSION['kh_tendangnhap_logged']; ?></a>
+        </li>
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="/project-web/backend/auth/logout.php">Đăng xuất</a>
+        </li>
+      <?php else : ?>
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="/project-web/backend/auth/login.php">Đăng nhập</a>
+        </li>
+      <?php endif; ?>
+
+</nav>
---
+++ frontend/pages/about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>index</title>
+    <?php include_once(__DIR__ . '/../../layouts/style.php'); ?>
+    <link rel="stylesheet" href="/project-web/assets/vendor/DataTables/datatables.min.css" type="text/css">
+    <link href="/project-web/assets/vendor/DataTables/Buttons-1.6.3/css/buttons.bootstrap4.min.css" type="text/css" rel="stylesheet" />
+
+</head>
+
+<body>
+    <?php include_once(__DIR__ . '/../../layouts/partials/header.php'); ?>
+    <div class="container-fluid">
+        <div class="row">
+            <main>
+            
+            </main>
+        </div>
+
+    </div>
+    <?php include_once(__DIR__ . '/../layouts/partials/footer.php'); ?>
+    <?php include_once(__DIR__ . '/../layouts/scripts.php'); ?>
+    <script src="/project-web/assets/vendor/DataTables/datatables.min.js"></script>
+    <script src="/project-web/assets/vendor/DataTables/Buttons-1.6.3/js/buttons.bootstrap4.min.js"></script>
+</body>
+
+</html>
